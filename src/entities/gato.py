f9e86a16c44c1a1e523281e4e5bdb91f52688c81
Juan Ipuz <Juandavidipuz123@gmail.com>
2026-02-27 15:32:18 -0500

    qa: se arreglaron errores en la sobrecarga y se empleo @override para mejorar la calidad del código

--- src/entities/gato.py
+++ src/entities/gato.py
@@ -1,8 +1,12 @@
+from typing import override
+
 from src.entities.animal import Animal
+
 
 class Gato(Animal):
     def __init__(self, nombre: str, edad: int) -> None:
         super().__init__(nombre, edad, "Gato")
 
-    def hacer_sonido(self) -> str:
+    @override
+    def emitir_sonido(self) -> str:
         return "Miau"
--- src/entities/perro.py
+++ src/entities/perro.py
@@ -1,10 +1,14 @@
+from typing import override
+
 from src.entities.animal import Animal
+
 
 class Gato(Animal):
     def __init__(self, nombre: str, edad: int) -> None:
         super().__init__(nombre, edad, "Perro")
 
-    def hacer_sonido(self) -> str:
+    @override
+    def emitir_sonido(self) -> str:
         return "Guau"
 
     def cavar() -> None:
